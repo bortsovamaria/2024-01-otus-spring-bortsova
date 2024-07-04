99dd8e999a1c957295f43f822fd3447508075995
Maria Bortsova <bortsovamariia@gmail.com>
2024-07-04 21:13:35 +0300

    add homework 18: fixed after review

--- homework-18/library-client/src/main/java/ru/otus/spring/libraryclient/feign/LibraryService.java
+++ homework-18/library-client/src/main/java/ru/otus/spring/libraryclient/feign/LibraryService.java
@@ -6,20 +6,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.otus.spring.libraryclient.dto.AuthorDto;
 import ru.otus.spring.libraryclient.dto.BookDto;
+import ru.otus.spring.libraryclient.dto.GenreDto;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 @FeignClient(name = "library-service")
-public interface LibraryServiceProxy {
-    @CircuitBreaker(name = "library-service", fallbackMethod = "fallbackGetAllBooks")
+public interface LibraryService {
+    @CircuitBreaker(name = "library-service", fallbackMethod = "buildFallbackBooks")
     @GetMapping(value = "/api/books")
     List<BookDto> getAllBooks();
 
-
-    default List<AuthorDto> fallbackGetAllBooks(Throwable throwable) {
-        return Collections.emptyList();
+    default List<BookDto> buildFallbackBooks(String animal) {
+        BookDto bookDto = new BookDto();
+        bookDto.setId(0L);
+        bookDto.setTitle("N/A");
+        bookDto.setAuthor(new AuthorDto(0L, "N/A"));
+        bookDto.setGenre(new GenreDto(0L, "N/A"));
+        return List.of(bookDto);
     }
-
 }
--- homework-18/library-client/src/main/java/ru/otus/spring/libraryclient/services/BookService.java
+++ homework-18/library-client/src/main/java/ru/otus/spring/libraryclient/services/BookService.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import ru.otus.spring.libraryclient.dto.BookDto;
-import ru.otus.spring.libraryclient.feign.LibraryServiceProxy;
+import ru.otus.spring.libraryclient.feign.LibraryService;
 
 import java.util.List;
 
@@ -15,7 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookService {
 
-    private final LibraryServiceProxy feignProxy;
+    private final LibraryService feignProxy;
 
     public List<BookDto> findAll() {
         return feignProxy.getAllBooks();
